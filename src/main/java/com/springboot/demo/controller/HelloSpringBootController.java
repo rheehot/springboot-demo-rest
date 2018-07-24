14655a1b09578a03548dd022787379918dca8df2
LAPTOP-29RV9SMV\rheeh <rheehot@gmail.com>
2018-07-24 17:06:47 +0900

    studying ....

---
+++ src/main/java/com/springboot/demo/controller/HelloSpringBootController.java
@@ -0,0 +1,8 @@
+package com.springboot.demo.controller;
+
+/**
+ * Created by Peter.L@gmail.com on 2018-07-24
+ * Github : http://github.com/rheehot
+ */
+public class HelloSpringBootController {
+}
